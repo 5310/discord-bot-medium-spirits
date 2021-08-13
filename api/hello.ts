b820cf06191f65d25a8d524f8ba5fdc1842e108f
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2021-08-13 17:05:09 +0530

    Renamed Specters to Spirits to keep things simple

--- api/hello.ts
+++ api/hello.ts
@@ -14,7 +14,7 @@ export default (request: VercelRequest, response: VercelResponse) => {
     })
   } catch (e) {
     response.status(400).json({
-      content: 'The Specter refuses to greet you...',
+      content: 'The spirit refuses to greet you...',
     })
   }
 }
