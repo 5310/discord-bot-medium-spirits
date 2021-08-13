84790f6e48dc87fa964060f09c6c4a51c44bc0cb
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2021-08-13 13:49:53 +0530

    Added a failsafe for mangled prompts

--- api/helloworld.ts
+++ api/helloworld.ts
@@ -3,12 +3,18 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 export default (request: VercelRequest, response: VercelResponse) => {
   console.debug(request.body)
 
-  const prompt = request.body.content.trim()
-  const regex = /^(hullo|hello|hi|hey) *(?<target>.*)/i
+  try {
+    const prompt = request.body.content.trim()
+    const regex = /^(hullo|hello|hi|hey) *(?<target>.*)/i
 
-  response.status(200).json({
-    content: `Hello from ${
-      prompt.match(regex).groups.target || 'the beyond!'
-    }!`,
-  })
+    response.status(200).json({
+      content: `Hello from ${
+        prompt.match(regex).groups.target || 'the beyond!'
+      }!`,
+    })
+  } catch (e) {
+    response.status(400).json({
+      content: 'The Specter refuses to greet you...',
+    })
+  }
 }
