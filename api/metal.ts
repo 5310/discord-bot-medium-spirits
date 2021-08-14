1b05c89f62beb81e4fe023250b765779e5cd6478
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2021-08-14 13:30:00 +0530

    Clean debug calls

--- api/metal.ts
+++ api/metal.ts
@@ -1,8 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  console.debug(request.body)
-
   try {
     response.status(200).send('🤘🤟')
   } catch (e) {
