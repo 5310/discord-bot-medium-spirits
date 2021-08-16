6ed5570a09dd6dc4791f782695e7cddad27c4aa7
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2021-08-16 20:31:23 +0530

    Add basic random playing-cards

---
+++ api/cards.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+const RANKS = [...'𝗔𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵𝗫𝗝𝗤𝗞']
+const SUITS = [...'♠♥♦♣']
+const JOKERS = '🃟🃏'
+
+const zip = (xs: unknown[], ys: unknown[]) =>
+  xs.flatMap((x) => ys.map((y) => `${x}${y}`))
+
+const shuffle = (xs: unknown[]) => {
+  const result = [] as unknown[]
+  for (let i = 0; i < xs.length; ++i) {
+    const j = Math.floor(Math.random() * (i + 1))
+    if (j !== i) result[i] = result[j]
+    result[j] = xs[i]
+  }
+  return result
+}
+
+const limit = (min: number, max: number, x: number) =>
+  x < min ? min : x > max ? max : x
+
+const DECK = [...zip(RANKS, SUITS), JOKERS, JOKERS]
+
+export default (request: VercelRequest, response: VercelResponse): void => {
+  try {
+    const count = limit(
+      1,
+      DECK.length,
+      parseInt(request.body.content.match(/(?<count>[0-9]+)/i)?.groups.count) ||
+        1,
+    )
+    const cards = shuffle(DECK).slice(0, count)
+    response.status(200).json({
+      content: cards.join(', '),
+    })
+  } catch (e) {
+    response.status(400).json({
+      content: 'The spirit refuses to greet you...',
+    })
+  }
+}
