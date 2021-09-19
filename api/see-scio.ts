d9a7063dfdd90beaff32228950774595cf844012
Sayantan Chaudhuri <5310@users.noreply.github.com>
2021-09-19 11:40:46 +0530

    Also detect dupespam

--- api/see-scio.ts
+++ api/see-scio.ts
@@ -6,7 +6,7 @@ export default (request: VercelRequest, response: VercelResponse): void => {
       request.body.content
         .replace(/[^s$5zck3<i1|eeyo0()°]/gi, '')
         .trim()
-        .match(/(s|\$|5|z)(c|k|<)(i|1|\||ee)(o|0|°|yo)/i) &&
+        .match(/(s|\$|5|z)+(c|k|<)+(i|1|\||ee)+(o|0|°|yo)+/i) &&
       !request.body.content.match(/scio/i)
     )
       response.status(200).send('😒') // ok
